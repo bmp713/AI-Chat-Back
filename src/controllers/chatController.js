2a77a1d68f3fcea8b6cb9e4ce1c7748fb94b31c0
unknown <bmp713@yahoo.com>
2025-06-01 10:58:39 -0700

    AI-Chat-Backend

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -19,8 +19,8 @@ class ChatController {
         }
     }
     async getResponse(message) {
-        //const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
-        const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
+        const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
+        //const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
 
         const headers = {
             'Content-Type': 'application/json'
